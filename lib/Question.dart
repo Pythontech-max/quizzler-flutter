3ba0f4389aa5b37b776f0025c3f7e2dc728e7655
Pythontech-max <57166376+Pythontech-max@users.noreply.github.com>
2022-01-15 23:23:05 +0530

    done

---
+++ lib/Question.dart
@@ -0,0 +1,12 @@
+class Question {
+  String questiontext;
+  bool   queationanswer;
+
+  Question(String q , bool a){
+   questiontext = q;
+   queationanswer = a;
+
+  }
+}
+
+Question newquestion = Question('true', true);
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
+import 'Quiz_Brain.dart';
+import 'Question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizbrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.grey.shade900,
+          body: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: QuizPage(),
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
+
+
+
 
 class QuizPage extends StatefulWidget {
   @override
@@ -25,6 +34,31 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [
+
+  ];
+
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  //
+  // ];
+  // List<bool> answers = [false , true ,true];
+
+  // Question q1 = Question('You can lead a cow down stairs but not up stairs.', false);
+
+  int questionnumber = 0;
+  List<Icons> l1 , l2 ,l3 = [];
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +71,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizbrain.getQuestionText(questionnumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +95,56 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctanswer = quizbrain.getCurrectAnswer(questionnumber);
+                if (correctanswer == true) {
+                  print('answer is right');
+                } else {
+                  print('answer is wrong');
+                }
+
+                setState(() {
+                  
+                  if (questionnumber < 12) {
+                    questionnumber++;
+                  } else {
+
+                    Alert(
+                      
+                      context: context,
+                      type: AlertType.error,
+                      title: "RFLUTTER ALERT",
+                      desc: "Flutter is more awesome with RFlutter Alert.",
+                      buttons: [
+                        DialogButton(
+                          child: Text(
+                            "COOL",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          onPressed: ()  {Navigator.pop(context ,true);},
+                          width: 120,
+                        )
+                      ],
+                    ).show();
+                  }
+
+                  if (correctanswer == true) {
+                    scorekeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.lightGreenAccent,
+                      ),
+                    );
+                  } else {
+                    scorekeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
+                  ;
+                });
+
                 //The user picked true.
               },
             ),
@@ -78,20 +162,57 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
+              onPressed: ()
+
+              {
+
+
+
+                bool correctanswer = quizbrain.getCurrectAnswer(questionnumber);
+                if (correctanswer == false) {
+                  print('answer is right');
+                } else {
+                  print('answer is wrong');
+                }
+
+                setState(() {
+                  if (questionnumber < 12) {
+                    questionnumber++;
+
+                  } else {
+
+                    questionnumber = 0;
+
+                  }
+
+                  if (correctanswer == false) {
+                    scorekeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.lightGreenAccent,
+                      ),
+                    );
+                  } else {
+                    scorekeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
+                }); //The user picked false.
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        )
+
+
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
